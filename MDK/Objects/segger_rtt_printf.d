4f629cbb13933ac572d5ec3aa3a5b6eb61951e8a
CopyPasteMaster <2298091947@qq.com>
2026-07-01 14:14:02 +0800

    清理编译中间文件

--- MDK/Objects/segger_rtt_printf.d
+++
@@ -1,6 +0,0 @@
-.\objects\segger_rtt_printf.o: ..\MIDDLE\SEGGER_RTT_V848\RTT\SEGGER_RTT_printf.c
-.\objects\segger_rtt_printf.o: ..\MIDDLE\SEGGER_RTT_V848\RTT\SEGGER_RTT.h
-.\objects\segger_rtt_printf.o: ..\MIDDLE\SEGGER_RTT_V848\Config\SEGGER_RTT_Conf.h
-.\objects\segger_rtt_printf.o: C:\Users\Administrator\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\segger_rtt_printf.o: C:\Users\Administrator\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
-.\objects\segger_rtt_printf.o: C:\Users\Administrator\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
